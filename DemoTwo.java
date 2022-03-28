e6470a7c25961416526d1d1cc6ac40bffe93c3be
damletanmay1993 <101171753+damletanmay1993@users.noreply.github.com>
2022-03-28 11:35:29 +0530

    Delete DemoTwo.java

--- DemoTwo.java
+++
@@ -1,9 +0,0 @@
-import org.testng.annotations.Test;
-
-public class DemoTwo {
-    @Test
-    public void TestCase() {
-        System.out.println("I'm in first test case from demoTwo Class");
-    }
-
-}
