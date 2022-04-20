5946fa5ae4d95284d432313e289c8727d1b7cd89
damletanmay1993 <101171753+damletanmay1993@users.noreply.github.com>
2022-04-20 09:50:50 +0530

    Add files via upload

---
+++ Appium/Activities/Activity2.java
@@ -0,0 +1,71 @@
+package examples;
+
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Activity2 {
+    WebDriverWait wait;
+    AndroidDriver<MobileElement> driver = null;
+
+    @BeforeClass
+    public void setup() throws MalformedURLException {
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("deviceName", "Appium");
+        caps.setCapability("platformName", "Android");
+        caps.setCapability("automationName", "UiAutomator2");
+        caps.setCapability("appPackage", "com.android.chrome");
+        caps.setCapability("appActivity", "com.google.android.apps.chrome.Main");
+        caps.setCapability("noReset", true);
+
+        driver = new AndroidDriver<>(new URL("http://0.0.0.0:4723/wd/hub"), caps);
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void testSearchAppium() {
+
+        driver.get("https://www.training-support.net/");
+
+
+        wait.until(ExpectedConditions.elementToBeClickable(MobileBy.AccessibilityId("About Us")));
+
+
+        String pageTitle = driver
+                .findElementByXPath("//android.widget.TextView[@text='Training Support']")
+                .getText();
+        System.out.println("Title on Homepage: " + pageTitle);
+
+
+        MobileElement aboutButton = driver.findElementByXPath("//android.view.View[@content-desc='About Us']");
+        aboutButton.click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+                MobileBy.xpath("//android.widget.TextView[@text='About Us']")
+        ));
+
+        String newPageTitle = driver
+                .findElementByXPath("//android.widget.TextView[@text='About Us']")
+                .getText();
+        System.out.println("Title on new page: " + newPageTitle);
+
+        Assert.assertEquals(pageTitle, "Training Support");
+        Assert.assertEquals(newPageTitle, "About Us");
+    }
+
+    @AfterTest
+    public void tearDown() {
+        driver.quit();
+    }
+}
